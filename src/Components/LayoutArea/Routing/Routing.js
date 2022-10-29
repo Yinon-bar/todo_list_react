b2d6fea3a00c9d31b2eafdd271c103ec0a4b1198
Yinon Bar <yinonbar1988@gmail.com>
2022-10-29 13:00:29 +0300

    todo list starting pack

--- src/Components/LayoutArea/Routing/Routing.js
+++ src/Components/LayoutArea/Routing/Routing.js
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "../../Pages/Home/Home";
 import Todo from "../../Pages/Todo/Todo";
+import Users from "../../Pages/Users/Users";
 
 function Routing() {
   return (
     <Routes>
       <Route path="/home" element={<Home />} />
       <Route path="/todo" element={<Todo />} />
+      <Route path="/users" element={<Users />} />
     </Routes>
   );
 }
