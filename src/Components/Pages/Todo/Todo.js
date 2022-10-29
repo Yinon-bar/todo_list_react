1519eeacf726e0b4091cb74ff3415e6e0fbbcad8
Yinon Bar <yinonbar1988@gmail.com>
2022-10-29 12:43:55 +0300

    todo list starting pack

---
+++ src/Components/Pages/Todo/Todo.js
@@ -0,0 +1,12 @@
+import "./Todo.css";
+
+function Todo() {
+  return (
+    <div className="Todo">
+      {/* Todo */}
+      <h1>Todo</h1>
+    </div>
+  );
+}
+
+export default Todo;
---
+++ src/Components/Pages/Users/Users.js
@@ -0,0 +1,12 @@
+import "./Users.css";
+
+function Users() {
+  return (
+    <div className="Users">
+      {/* Users */}
+      <h1>Users</h1>
+    </div>
+  );
+}
+
+export default Users;
